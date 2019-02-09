21798d01de2ad414078073d4b81aea182dbc8324
darioaplicano <aplicano0921@gmail.com>
2019-02-09 11:47:54 -0600

    Completado, desarrollo de la función quick_sort que ordena un vector

---
+++ Practicas/quick_sort.scala
@@ -0,0 +1,58 @@
+//Implementa quick_sort para ordenar un vector que no esté ordenado
+
+def ordenarR(lista: List[Int], cabeza: Int):List[Int]={
+    if(lista.isEmpty){
+        List(cabeza)
+    }else{
+        if(cabeza < lista.head){
+            return cabeza::lista
+        }else{
+            lista.head::ordenarR(lista.tail,cabeza)
+        }
+    }
+}
+
+def recorridoR(lista: List[Int], cabeza:Int):List[Int]={
+    if(lista.isEmpty){
+        List(cabeza)
+    }else{
+        val lista2 = recorridoR(lista.tail, lista.head)
+        if(lista2.forall(x => cabeza < x)){
+            return cabeza::lista2
+        }else{
+            ordenarR(lista2,cabeza)
+        }
+    }
+}
+
+def quick_sort(lista: List[Int]):List[Int]={
+    if(lista.isEmpty){
+        return lista
+    }else{
+        recorridoR(lista.tail,lista.head)
+    }
+}
+
+println("")
+println("Primer caso, una lista vacia")
+val lista = List()
+println("Resultado")
+quick_sort(lista)
+
+println("")
+println("Segundo caso, una lista de un elemento")
+val lista2 = List(1)
+println("Resultado")
+quick_sort(lista2)
+
+println("")
+println("Tercer caso, una lista de dos elementos ordenados")
+val lista3 = List(1,4, 5, 7, 9)
+println("Resultado")
+quick_sort(lista3)
+
+println("")
+println("Tercer caso, una lista de dos elementos desordenados")
+val lista3 = List(2,1,6,3,5,9,0)
+println("Resultado")
+quick_sort(lista3)
