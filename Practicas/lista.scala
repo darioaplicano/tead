37f149960eb96be97ec928998717cf0275304809
darioaplicano <aplicano0921@gmail.com>
2019-02-09 09:42:19 -0600

    Completado, agregar un numero a una lista ordenada

--- Practicas/lista.scala
+++ Practicas/lista.scala
@@ -1,3 +1,7 @@
+//Debo insertar un entero en una lista de enteros, que puede estar vacía.
+//Si la lista de origen no está ordenada, debe devolver falso y la lista vacía.
+//Si está ordenada, debe devolver cierto y la nueva lista con el elemento
+//ordenado correctamente.
 def insertarR(lista: List[Int], cabeza: Int, numero: Int, agregado: Boolean):List[List[AnyVal]]={
     if(lista.isEmpty){
         if(numero > cabeza){
@@ -7,20 +11,25 @@ def insertarR(lista: List[Int], cabeza: Int, numero: Int, agregado: Boolean):Lis
         }
     }else{
         if(lista.forall(x => x > cabeza)){
-            val p = insertarR(lista.tail,lista.head,numero, false)
-            val lista2 = p(1)
-            if(lista2.head == lista.head){
-                List(List(true),cabeza::lista2)
-            }else{
-                if(cabeza > lista2.head){
-                    List(List(true),lista2.head::cabeza::lista2.tail)
-                }else{
+            val resultado = insertarR(lista.tail,lista.head,numero, false)
+            if(resultado(0).head == true){
+                val lista2 = resultado(1)
+                val cabezaLista2 = lista2.head.asInstanceOf[Number].longValue
+                if(lista2.head == lista.head){
                     List(List(true),cabeza::lista2)
+                }else{
+                    if(cabeza > cabezaLista2){
+                        List(List(true),lista2.head::cabeza::lista2.tail)
+                    }else{
+                        List(List(true),cabeza::lista2)
+                    }
                 }
+            }else{
+                List(List(false),Nil)
             }
         }
         else{
-            println("La lista no está ordenada")
+            println("La lista no esta ordenada")
             List(List(false),Nil)
         } 
     }
@@ -34,6 +43,17 @@ def insertar(lista: List[Int], numero: Int):List[List[AnyVal]]={
     }
 }
 
-
+println("")
+println("Primer caso, agregar el numero 3 a una lista vacia")
 val lista = List()
 insertar(lista,3)
+
+println("")
+println("Segundo caso, agregar el numero 3 a una lista desordenada")
+val lista2 = List(1,5,2,4,6)
+insertar(lista2,3)
+
+println("")
+println("Tercer caso, agregar el numero 3 a una lista ordenada")
+val lista3 = List(1,2,4,5,6)
+insertar(lista3,3)
