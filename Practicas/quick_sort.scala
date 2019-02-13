5ce5a9f961430b9241d99e54663225858156ea59
darioaplicano <aplicano0921@gmail.com>
2019-02-13 16:28:44 -0600

    Cambio de ordenamiento secuencial a quicksort

--- Practicas/quick_sort.scala
+++ Practicas/quick_sort.scala
@@ -1,58 +1,24 @@
 //Implementa quick_sort para ordenar un vector que no esté ordenado
 
-def ordenarR(lista: List[Int], cabeza: Int):List[Int]={
-    if(lista.isEmpty){
-        List(cabeza)
+def repetidosR(repetidos: Int, ultimo: Int):List[Int]={
+    if(repetidos == 1){
+        List(ultimo)
     }else{
-        if(cabeza < lista.head){
-            return cabeza::lista
-        }else{
-            lista.head::ordenarR(lista.tail,cabeza)
-        }
+        ultimo::repetidosR((repetidos-1),ultimo)
     }
 }
 
-def recorridoR(lista: List[Int], cabeza:Int):List[Int]={
-    if(lista.isEmpty){
-        List(cabeza)
+def quick_sortR(lista: List[Int]):List[Int]={
+    if(lista.length == 1 | lista.isEmpty){
+        lista
     }else{
-        val lista2 = recorridoR(lista.tail, lista.head)
-        if(lista2.forall(x => cabeza < x)){
-            return cabeza::lista2
-        }else{
-            ordenarR(lista2,cabeza)
-        }
+        val ultimo = lista(lista.length-1)
+        val repetidos = lista.count(_==ultimo)
+        val izquierda = quick_sortR(lista.filter( x => x < ultimo))
+        val derecha = quick_sortR(lista.filter( x => x > ultimo))
+        List(izquierda, repetidosR(repetidos, ultimo), derecha).flatten
     }
 }
 
-def quick_sort(lista: List[Int]):List[Int]={
-    if(lista.isEmpty){
-        return lista
-    }else{
-        recorridoR(lista.tail,lista.head)
-    }
-}
-
-println("")
-println("Primer caso, una lista vacia")
-val lista = List()
-println("Resultado")
-quick_sort(lista)
-
-println("")
-println("Segundo caso, una lista de un elemento")
-val lista2 = List(1)
-println("Resultado")
-quick_sort(lista2)
-
-println("")
-println("Tercer caso, una lista de dos elementos ordenados")
-val lista3 = List(1,4, 5, 7, 9)
-println("Resultado")
-quick_sort(lista3)
-
-println("")
-println("Tercer caso, una lista de dos elementos desordenados")
-val lista3 = List(3,7,8,5,2,1,9,5,4)
-println("Resultado")
-quick_sort(lista3)
+val lista = List(6, 5, 3, 1, 8, 7, 4, 0, 9, 20, 21, 2, 1, 0)
+quick_sortR(lista)
