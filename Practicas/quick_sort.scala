b937193ae26941653f17640960430d9d7294abed
darioaplicano <aplicano0921@gmail.com>
2019-02-20 00:28:05 +0100

    Mejorado el método quick_sort al considerar valores repetidos en la list

--- Practicas/quick_sort.scala
+++ Practicas/quick_sort.scala
@@ -1,22 +1,14 @@
 //Implementa quick_sort para ordenar un vector que no esté ordenado
 
-def repetidosR(repetidos: Int, ultimo: Int):List[Int]={
-    if(repetidos == 1){
-        List(ultimo)
-    }else{
-        ultimo::repetidosR((repetidos-1),ultimo)
-    }
-}
-
 def quick_sortR(lista: List[Int]):List[Int]={
     if(lista.length == 1 | lista.isEmpty){
         lista
     }else{
         val ultimo = lista(lista.length-1)
-        val repetidos = lista.count(_==ultimo)
+        val repetidos = lista.filter( x => x == ultimo)
         val izquierda = quick_sortR(lista.filter( x => x < ultimo))
         val derecha = quick_sortR(lista.filter( x => x > ultimo))
-        List(izquierda, repetidosR(repetidos, ultimo), derecha).flatten
+        List(izquierda, repetidos, derecha).flatten
     }
 }
 
