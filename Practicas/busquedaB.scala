4ad00b5e0ca548caa255c0768c23c3699ab7a5e4
darioaplicano <aplicano0921@gmail.com>
2019-02-20 00:28:28 +0100

    Corregida la búsqueda binaria (ahora si lo es)

--- Practicas/busquedaB.scala
+++ Practicas/busquedaB.scala
@@ -1,99 +1,36 @@
-import math._
 
-def busquedaBR(arreglo: Array[Double], numero: Double):(Int, List[Double])={
-    val mitad = floor(arreglo.length / 2).toInt
-    val mitad1 = mitad+1
-
-    if(arreglo.length == 1){
-        if( arreglo(0) > numero){
-            return (-1, List(numero, arreglo(0)))
-        }else{
-            return (-1, List(arreglo(0), numero))
-        }
-    }else{
-        if(arreglo.length == 2){
-            if(arreglo(mitad) < numero){
-                return (-1, List(arreglo(1), numero))
-            }else{
-                if(arreglo(0) < numero){
-                    return (-1, List(arreglo(0), arreglo(1)))
-                }else{
-                    return (-1, List(numero, arreglo(0)))
-                }
+def busquedaB(lista: List[Double], elemento: Double):(Int, Int) ={
+    def busquedaBR(lista: List[Double], posicion: Int, movimiento: Boolean):(Int, Int) = {
+        if(lista.isEmpty)
+            (-1, if(movimiento == false) posicion - 1 else posicion)
+        else{
+            val mitad = (lista.length/2).toInt
+            if(elemento > lista(mitad)){
+                val listab = lista.filter(x => lista.indexOf(x) > mitad)
+                busquedaBR(listab, posicion + (listab.length/2).toInt + 1, true)
             }
-        }else{
-            if(arreglo(mitad) < numero){
-                if(arreglo(mitad1) > numero){
-                        (-1, List(arreglo(mitad), arreglo(mitad1)))
-                }else{
-                    val arregloM = arreglo.filter(x => arreglo.indexOf(x) >= mitad)
-                    busquedaBR(arregloM, numero)
+            else
+                if(elemento < lista(mitad)){
+                    val listab = lista.filter(x => lista.indexOf(x) < mitad)
+                    busquedaBR(listab, posicion - (listab.length/2).toInt, false)
                 }
-            }else{
-                val arregloM = arreglo.filter(x => arreglo.indexOf(x) <= mitad)
-                busquedaBR(arregloM, numero)
-            }
+                else
+                    (if(mitad == 0 & movimiento == false) posicion - 1 else posicion, -1)
         }
     }
+    if(lista.isEmpty)
+        (-1, -1)
+    else
+        busquedaBR(lista, (lista.length/2).toInt, true)
 }
 
-def busqueda(arreglo: Array[Double], numero: Double):(Int, List[Double])={
-    if(arreglo.isEmpty){
-        return (-1, Nil)
-    }else{
-        if(arreglo.indexOf(numero) == (-1)){
-            println(numero)
-            busquedaBR(arreglo, numero)
-        }else{
-            return (arreglo.indexOf(numero)+1, Nil)
-        }
-    }
-}
-
-println()
-println("Primer caso, buscar un elemento que se encuentra sobre el arreglo ordenado")
-val arreglo = Array(1.2, 2.3, 3.2, 5.7, 7.8, 8.2)
-busqueda(arreglo, 5.7)
-
-println()
-println("Segundo caso, buscar un elemento que no se encuentra sobre el arreglo ordenado")
-busqueda(arreglo, 6)
-
-println()
-println("Tercer caso, buscar un elemento que no se encuentra sobre el arreglo ordenado")
-busqueda(arreglo, 5.0)
-
-println()
-println("Cuarto caso, buscar un elemento que no se encuentra sobre el arreglo ordenado")
-busqueda(arreglo, 8.0)
-
-println()
-println("Quinto caso, buscar un elemento que no se encuentra sobre el arreglo ordenado")
-busqueda(arreglo, 2)
-
-println()
-println("Sexto caso, buscar un elemento que no se encuentra sobre el arreglo ordenado")
-busqueda(arreglo, 0)
-
-println()
-println("Septimo caso, buscar un elemento que no se encuentra sobre el arreglo ordenado")
-busqueda(arreglo, 9)
-
-println()
-println("Octavo caso, buscar un elemento que no se encuentra sobre el arreglo ordenado")
-busqueda(arreglo, 10)
-
-println()
-println("Noveno caso, buscar un elemento que no se encuentra sobre el arreglo de un elemento")
-val arreglo2 = Array(1.0)
-busqueda(arreglo2, 0)
-
-println()
-println("Decimo primero caso, buscar un elemento que no se encuentra sobre el arreglo de un elemento")
-val arreglo2 = Array(1.0)
-busqueda(arreglo2, 2)
-
-println()
-println("Decimo segundo caso, buscar un elemento que no se encuentra sobre el arreglo vacío")
-val arreglo2 = Array.empty[Double]
-busqueda(arreglo2, 2)
+val lista = List(1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59.0)
+busquedaB(lista, 0)
+busquedaB(lista, 1)
+busquedaB(lista, 3)
+busquedaB(lista, 19)
+busquedaB(lista, 23)
+busquedaB(lista, 29)
+busquedaB(lista, 53)
+busquedaB(lista, 59)
+busquedaB(lista, 60)
